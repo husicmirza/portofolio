9f050bf28e339787703c7b56b02fe732154005e6
husicmirza <mirza.huc@gmail.com>
2024-09-21 10:10:49 +0300

    Add recent project section with 3d-pin card

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,7 +1,64 @@
+import { projects } from "@/data";
 import React from "react";
+import { PinContainer } from "./ui/3d-pin";
+import { FaLocationArrow } from "react-icons/fa";
 
 const RecentProjects = () => {
-  return <div>RecentProjects</div>;
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading">
+        A showcase of <span className="text-purple">recent projects</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
+        {projects.map((project) => (
+          <div
+            key={project.id}
+            className="lg:min-h-[32.5rem] h-[28rem] flex items-center justify-center w-[80vw] sm:h-[41rem] sm:w-[570px]"
+          >
+            <PinContainer title={project.link} className="" href={project.link}>
+              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[30vh] mb-10">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                  <img src="/bg.png" alt="bg-img" />
+                </div>
+                <img
+                  src={project.img}
+                  alt={project.title}
+                  className="z-10 absolute bottom-0"
+                />
+              </div>
+              <div className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+                {project.title}
+              </div>
+              <p className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2">
+                {project.des}
+              </p>
+              <div className="flex items-center justify-between mt-7 mb-3">
+                <div className="flex items-center">
+                  {project.iconLists.map((icon, index) => (
+                    <div
+                      className="border border-white/[0.2] flex items-center rounded-full justify-center bg-black-100 lg:w-10 lg:h-10 w-8 h-8"
+                      key={index}
+                      style={{
+                        transform: `translateX(-${5 * index + 2}px)`,
+                      }}
+                    >
+                      <img src={icon} alt={icon} className="p-2" />
+                    </div>
+                  ))}
+                </div>
+                <div className="flex items-center justify-center rounded-3xl">
+                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
+                    Check Live Site
+                  </p>
+                  <FaLocationArrow className="ms-3" color="#CBACF9" />
+                </div>
+              </div>
+            </PinContainer>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RecentProjects;
--- data/index.ts
+++ data/index.ts
@@ -8,13 +8,14 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "Tech enthusiast with a passion for development.",
     description: "",
-    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    className:
+      "lg:col-span-3 md:col-span-6 md:row-span-4 min-h-[40vh] lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/b1.svg",
-    spareImg: "",
+    img: "/grid.svg",
+    spareImg: "/me1.svg",
   },
   {
     id: 2,
@@ -66,5 +67,40 @@ export const gridItems = [
     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
     img: "",
     spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System Planets to Explore",
+    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Image SaaS - Canva Application",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Animated Apple Iphone 3D Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
   },
 ];
