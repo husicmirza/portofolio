fcc7c3f087afcaed8c920bd24be7671c239e2200
husicmirza <mirza.huc@gmail.com>
2024-09-22 17:58:00 +0300

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,19 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mirza's Portofolio",
-  description: "Modern & Minimalist Portofolio",
+  title: "Mirza Husic Portofolio",
+  description:
+    "Mirza Husic, a skilled Software Engineer specializing in React and Next.js, focused on creating fast, responsive, and user-friendly web applications",
+  icons: "/mh.png",
+  openGraph: {
+    siteName: "mirzahusic.dev",
+    url: `https://mirzahusic.dev`,
+    title: "Mirza Husic Portofolio",
+    description:
+      "Mirza Husic, a skilled Software Engineer specializing in React and Next.js, focused on creating fast, responsive, and user-friendly web applications",
+    images: ["/me.png"],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
